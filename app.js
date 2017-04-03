cc2ba68d47767992fc9da4b122c70f09e0746870
schoeu <quanyou1013@gmail.com>
2017-04-03 20:45:16 +0800

    delete ico module.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -19,8 +18,6 @@ var staticDir = path.join(__dirname, 'themes', theme);
 app.set('views', path.join(staticDir, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,6 +27,7 @@ app.use(express.static(path.join(staticDir, 'static')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/tags', tags);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
