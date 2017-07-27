3fe31cefcbc39ebad65c77ded7da85d92060e62d
liuquanyou <liuquanyou@baidu.com>
2017-07-28 00:41:19 +0800

    add gulp config file.

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,47 @@ var path = require('path');
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var minify = require('gulp-minify-css');
+var livereload = require('gulp-livereload')
 
 var config = require(path.join(__dirname, './src/config')).path(path.join(__dirname, './config/config_app'));
 var theme = path.join('./themes', config.getItem('theme'), 'static');
 var themePath = path.join(theme, 'dict');
+
+var srcOpts = {
+    base: __dirname,
+    buffer: true
+};
+
 // 默认任务
 gulp.task('default', ['mini']);
 
 
-var a = gulp.src([theme + '/js/*.js', '!' + theme + '/js/*.min.js']);
-console.log(themePath + '/js/');
 // 压缩js/css任务
 gulp.task('mini', function () {
-
+    console.log('mini task.');
     // 压缩js
-    gulp.src([theme + '/js/*.js', '!' + theme + '/js/*.min.js'])
+    gulp.src([theme + '/js/*.js', '!' + theme + '/js/*.min.js'], srcOpts)
         .pipe(uglify())
         .pipe(gulp.dest(themePath + '/js/'));
 
     // 压缩css
     gulp.src([theme + '/css/*.css', '!' + theme + '/css/*.min.css'])
         .pipe(minify())
-        .pipe(gulp.dest(themePath + '/css/'));
+        .pipe(gulp.dest(themePath + '/css/')).pipe(livereload());
+});
+
+gulp.task('watch', function () {
+    livereload.listen();
+
+    // 监听目录下的文件，若文件发生变化，则调用mini任务。
+    gulp.watch(
+        [
+            theme + '/js/*.js',
+            '!' + theme + '/js/*.min.js',
+            theme + '/css/*.css',
+            '!' + theme + '/css/*.min.css'
+        ]
+        , ['mini']
+    );
 });
 
